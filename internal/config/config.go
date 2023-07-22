75ce83906edcc637c8f01633d66bbb17b555c062
Andranik <tednaaa@gmail.com>
2023-07-22 18:17:00 +0400

    feat: add config

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Env         string `yaml: "env" env-required: "true"`
+	StoragePath string `yaml: "storage_path" env-required: "true"`
+	HTTPServer  `yaml: "http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml: "address" env-required: "true"`
+	Timeout     time.Duration `yaml: "timeout" env-required: "true"`
+	IdleTimeout time.Duration `yaml: "idle_timeout" env-required: "true"`
+}
+
+func MustLoad() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(configPath, &config); err != nil {
+		log.Fatalf("cannot read config %s", err)
+	}
+
+	return &config
+}
