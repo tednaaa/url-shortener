d7c0d49c5728f435c6be1328ab270eeb21d9f432
Andranik <tednaaa@gmail.com>
2023-07-22 22:31:55 +0400

    feat: add sqlite storage

---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,90 @@
+package sqlite
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"url-shortener/internal/storage"
+
+	"github.com/mattn/go-sqlite3"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const operation = "storage.sqlite.New"
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", operation, err)
+	}
+
+	stmt, err := db.Prepare(`
+		CREATE TABLE IF NOT EXISTS url(
+			id INTEGER PRIMARY KEY,
+			alias TEXT NOT NULL UNIQUE,
+			url TEXT NOT NULL
+		);
+		CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
+	`)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", operation, err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", operation, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveUrl(urlToSave string, alias string) (int64, error) {
+	const operation = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", operation, err)
+	}
+
+	response, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", operation, storage.ErrorUrlExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", operation, err)
+	}
+
+	id, err := response.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id %w", operation, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) GetUrl(alias string) (string, error) {
+	const operation = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+
+	if err != nil {
+		return "", fmt.Errorf("%s: prepare statement %w", operation, err)
+	}
+
+	var responseUrl string
+
+	err = stmt.QueryRow(alias).Scan(&responseUrl)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", storage.ErrorUrlNotFound
+		}
+
+		return "", fmt.Errorf("%s: execute statement %w", operation, err)
+	}
+
+	return responseUrl, nil
+}
